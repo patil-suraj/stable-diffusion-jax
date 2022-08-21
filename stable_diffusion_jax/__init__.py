1207913515e9f0fdaa40778a8cdde17e30f98c5b
patil-suraj <surajp815@gmail.com>
2022-08-21 19:40:30 +0530

    style

--- stable_diffusion_jax/__init__.py
+++ stable_diffusion_jax/__init__.py
@@ -2,5 +2,5 @@ from .configuration_unet2d import UNet2DConfig
 from .configuration_vae import VAEConfig
 from .modeling_unet2d import UNet2D
 from .modeling_vae import AutoencoderKL
-from .scheduling_pndm import PNDMScheduler
 from .pipeline_stable_diffusion import StableDiffusionPipeline
+from .scheduling_pndm import PNDMScheduler
