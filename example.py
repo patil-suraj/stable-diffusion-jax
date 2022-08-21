d33dca718f49835299e17beb2d350a0f2f5c3b96
patil-suraj <surajp815@gmail.com>
2022-08-21 23:52:31 +0530

    finish example

--- example.py
+++ example.py
@@ -1,50 +1,88 @@
 import jax
+import jax.numpy as jnp
 import numpy as np
-
+from flax.jax_utils import replicate
+from flax.training.common_utils import shard
 from PIL import Image
+from transformers import CLIPTokenizer, FlaxCLIPTextModel
 
-from transformers import FlaxCLIPTextModel, CLIPTokenizer
-
-from stable_diffusion_jax import StableDiffusionPipeline, UNet2D, AutoencoderKL, PNDMScheduler
-from stable_diffusion_jax.pipeline_stable_diffusion import InferenceState
+from stable_diffusion_jax import (
+    AutoencoderKL,
+    InferenceState,
+    PNDMScheduler,
+    StableDiffusionPipeline,
+    UNet2D
+)
 from stable_diffusion_jax.convert_diffusers_to_jax import convert_diffusers_to_jax
+
 
 # convert diffusers checkpoint to jax
 pt_path = "path_to_diffusers_pt_ckpt"
 fx_path = "save_path"
 convert_diffusers_to_jax(pt_path, fx_path)
 
+
 # inference with jax
-clip_model, clip_params = FlaxCLIPTextModel.from_pretrained("openai/clip-vit-large-patch14", _do_init=False)
-unet, unet_params = UNet2D.from_pretrained(f"{fx_path}/unet", _do_init=False)
-vae, vae_params = AutoencoderKL.from_pretrained(f"{fx_path}/vae", _do_init=False)
+dtype = jnp.bfloat16
+clip_model, clip_params = FlaxCLIPTextModel.from_pretrained(
+    "openai/clip-vit-large-patch14", _do_init=False, dtype=dtype
+)
+unet, unet_params = UNet2D.from_pretrained(f"{fx_path}/unet", _do_init=False, dtype=dtype)
+vae, vae_params = AutoencoderKL.from_pretrained(f"{fx_path}/vae", _do_init=False, dtype=dtype)
 
 tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
 scheduler = PNDMScheduler()
 
 
-state = InferenceState(text_encoder_params=clip_params, unet_params=unet_params, vae_params=vae_params)
-# for single GPU 
-state = jax.tree_map(lambda arr: jax.device_put(arr, jax.devices()[0]), state)
+# create inference state and replicate it across all TPU devices
+inference_state = InferenceState(text_encoder_params=clip_params, unet_params=unet_params, vae_params=vae_params)
+inference_state = replicate(inference_state)
 
 
+# create pipeline
 pipe = StableDiffusionPipeline(text_encoder=clip_model, tokenizer=tokenizer, unet=unet, scheduler=scheduler, vae=vae)
 
+
+
+# prepare inputs
+num_samples = 8
 p = "A cinematic film still of Morgan Freeman starring as Jimi Hendrix, portrait, 40mm lens, shallow depth of field, close up, split lighting, cinematic"
-input_ids = tokenizer(p, padding="max_length", truncation=True, max_length=77, return_tensors="jax").input_ids
-uncond_input_ids = tokenizer([""], padding="max_length", truncation=True, max_length=77, return_tensors="jax").input_ids
-rng = jax.random.PRNGKey(42)
+
+input_ids = tokenizer(
+    [p] * num_samples, padding="max_length", truncation=True, max_length=77, return_tensors="jax"
+).input_ids
+uncond_input_ids = tokenizer(
+    [""] * num_samples, padding="max_length", truncation=True, max_length=77, return_tensors="jax"
+).input_ids
+prng_seed = jax.random.PRNGKey(42)
+
+# shard inputs and rng
+input_ids = shard(input_ids)
+uncond_input_ids = shard(uncond_input_ids)
+prng_seed = jax.random.split(prng_seed, 8)
 
 
-# image = pipe.sample(
-#     input_ids, uncond_input_ids, prng_seed=rng, inference_state=state, guidance_scale=7.5, num_inference_steps=50
-# )
+# pmap the sample function
+num_inference_steps = 50
+guidance_scale = 1.0
 
-# with jit
-sample = jax.jit(pipe.sample, static_argnums=4)
-images = sample(input_ids, uncond_input_ids, prng_seed=rng, inference_state=state, guidance_scale=7.5, num_inference_steps=50)
+sample = jax.pmap(pipe.sample, static_broadcasted_argnums=(4, 5))
 
-images = np.asarray((images / 2 + 0.5))
-images = np.clip(images, 0, 1)
+# sample images
+images = sample(
+    input_ids,
+    uncond_input_ids,
+    prng_seed,
+    inference_state,
+    num_inference_steps,
+    guidance_scale,
+)
+
+
+# convert images to PIL images
+images = images / 2 + 0.5
+images = jnp.clip(images, 0, 1)
 images = (images * 255).round().astype("uint8")
+images = np.asarray(images).reshape((num_samples, 512, 512, 3))
+
 pil_images = [Image.fromarray(image) for image in images]
