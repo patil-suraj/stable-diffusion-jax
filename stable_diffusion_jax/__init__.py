07e69e511bd69eaf30a7b04960ee0e09f345a6d9
patil-suraj <surajp815@gmail.com>
2022-08-21 23:52:24 +0530

    add InferenceState om init

--- stable_diffusion_jax/__init__.py
+++ stable_diffusion_jax/__init__.py
@@ -2,5 +2,5 @@ from .configuration_unet2d import UNet2DConfig
 from .configuration_vae import VAEConfig
 from .modeling_unet2d import UNet2D
 from .modeling_vae import AutoencoderKL
-from .pipeline_stable_diffusion import StableDiffusionPipeline
+from .pipeline_stable_diffusion import InferenceState, StableDiffusionPipeline
 from .scheduling_pndm import PNDMScheduler
