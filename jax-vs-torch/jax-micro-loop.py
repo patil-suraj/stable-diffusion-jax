6705c7bab248ce372f2f65f0a1dcf2ff01fda1ab
Pedro Cuenca <pedro@huggingface.co>
2022-08-31 13:31:56 +0200

    Change path, make some options configurable.

--- jax-vs-torch/jax-micro-loop.py
+++ jax-vs-torch/jax-micro-loop.py
@@ -1,19 +1,38 @@
 import jax
+import jax.numpy as jnp
+import os
+
+device = "cuda"
+run_parallel = True            # Set to False for interactive / debugging
+dtype = jnp.float32
+enable_x64_timesteps = True
+
+if enable_x64_timesteps:
+    # Experimental: enable int64 for timestep.
+    # This will perform some computations in float64, then we'll truncate to float32.
+    jax.config.update("jax_enable_x64", True)
+
+if device == "cpu":
+    # Make sure we really use the CPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    jax.config.update('jax_platform_name', 'cpu')
+
 num_devices = jax.device_count()
 device_type = jax.devices()[0].device_kind
 
 print(f"Found {num_devices} JAX devices of type {device_type}.")
-assert device_type.startswith("TPU"), "Available device is not a TPU, please select TPU from Edit > Notebook settings > Hardware accelerator"
+# assert device_type.startswith("TPU"), "Available device is not a TPU, please select TPU from Edit > Notebook settings > Hardware accelerator"
 
-import jax.numpy as jnp
 from flax.jax_utils import replicate
 from flax.training.common_utils import shard
 
+import numpy as np
+
+from PIL import Image
+from stable_diffusion_jax.scheduling_pndm import PNDMSchedulerState
 from stable_diffusion_jax import (
     AutoencoderKL,
-    InferenceState,
     PNDMScheduler,
-    StableDiffusionPipeline,
     UNet2D
 )
 
@@ -21,26 +40,30 @@ from tqdm import tqdm
 import pickle
 
 # Local checkout
-flax_path = "sd-v1-4-flax"
+flax_path = "/sddata/sd-v1-4-flax"
+tensors_path = "/sddata/sd-tests/tensors"
 
-dtype = jnp.float32
 unet, unet_params = UNet2D.from_pretrained(f"{flax_path}/unet", _do_init=False, dtype=dtype)
 scheduler = PNDMScheduler.from_config(f"{flax_path}/scheduler")
+initial_state = scheduler.state.state_dict.copy()
 
 # Using jax.debug.print() makes it crash :()
 def mini_sample(
     text_embeddings: jnp.ndarray,
     latents: jnp.ndarray,
     unet_params,
+    scheduler_state_dict: dict,
     num_inference_steps: int = 50,
+    break_after: int = 51,
 ):
-    scheduler.set_timesteps(num_inference_steps, offset=1)
+    scheduler_state = PNDMSchedulerState.from_state_dict(scheduler_state_dict)
+    scheduler_state = scheduler.set_timesteps(scheduler_state, latents.shape, num_inference_steps, offset=1)
+    scheduler_state_dict = scheduler_state.state_dict
 
-    def loop_body(step, latents):
-        t = jnp.array(scheduler.timesteps)[step]
+    def loop_body(step, args):
+        latents, scheduler_state_dict = args
+        t = jnp.array(scheduler_state_dict["timesteps"], dtype=jnp.int64)[step]
         timestep = jnp.broadcast_to(t, latents.shape[0])
-
-#         jax.debug.print(f"🤯 {t}")
         
         # predict the noise residual
         noise_pred = unet(
@@ -48,41 +71,83 @@ def mini_sample(
         )
 
         # compute the previous noisy sample x_t -> x_t-1
-        latents = scheduler.step(noise_pred, t, latents)["prev_sample"]
-        return latents
+        latents, scheduler_state_dict = scheduler.step(scheduler_state_dict, noise_pred, t, latents)
+        latents = jnp.array(latents["prev_sample"], dtype=jnp.float32)
+        return latents, scheduler_state_dict
 
-    latents = jax.lax.fori_loop(0, num_inference_steps, loop_body, latents)
+    n = min(len(scheduler_state.timesteps), break_after)
+    if run_parallel:
+        latents, scheduler_state_dict = jax.lax.fori_loop(0, n, loop_body, (latents, scheduler_state_dict))
+    else:
+        for step in range(n):
+            latents, scheduler_state_dict = loop_body(step, (latents, scheduler_state_dict))
+            print(f"{step}: {latents[0, 0, 0, 1]}")
+
+    return latents, scheduler_state_dict
+
+p_sample = jax.pmap(mini_sample, in_axes=(0, 0, 0, None), static_broadcasted_argnums=(4,5))
+
+# Run tests on a single device
+devices = jax.devices(device)[:1]
+
+def read_latents():
+    with open(f'{tensors_path}/latents_7667_cuda', 'rb') as f:
+        latents = pickle.load(f)
+
+    latents = jnp.array(latents)
+    latents = jnp.transpose(latents, (0, 2, 3, 1))
     return latents
 
-p_sample = jax.pmap(mini_sample, static_broadcasted_argnums=(3,))
+latents = read_latents()
 
-# Run tests on a single device: "tpu" or "cpu".
-# Running iteratively is very slow, so `shard` and `replicate` anyway.
-
-devices = jax.devices("tpu")[:1]
-
-unet_params = replicate(unet_params, devices)
-with open('tensors/latents_7667', 'rb') as f:
-    latents = pickle.load(f)
-
-latents = jnp.array(latents)
-latents = jnp.transpose(latents, (0, 2, 3, 1))
-
-with open('tensors/embeddings_7667', 'rb') as f:
+with open(f'{tensors_path}/embeddings_7667', 'rb') as f:
     text_embeddings = pickle.load(f)
-
 text_embeddings = jnp.array(text_embeddings)
 
-text_embeddings = jax.device_put_sharded([text_embeddings], devices)
-latents = jax.device_put_sharded([latents], devices)
+if run_parallel:
+    latents = jax.device_put_sharded([latents], devices)
+    unet_params = replicate(unet_params, devices)
+    text_embeddings = jax.device_put_sharded([text_embeddings], devices)
 
-latents = p_sample(text_embeddings, latents, unet_params, 50)
+num_inference_steps = 50
+sample_fn = p_sample if run_parallel else mini_sample
+for step in [50]:
+    latents = read_latents()
+    if run_parallel:
+        latents = jax.device_put_sharded([latents], devices)
+
+    scheduler_state_dict = initial_state.copy()
+    latents, _ = sample_fn(text_embeddings, latents, unet_params, scheduler_state_dict, num_inference_steps, step+1)
+
+    latents = latents[0]        # unshard
+    slice = latents[0, 0, 0, :] if run_parallel else latents[0, 0, :]
+    print(f"Step: {step}: {slice}")
+
+
+# scheduler_state_dict = initial_state.copy()
+# latents, _ = p_sample(text_embeddings, latents, unet_params, scheduler_state_dict, num_inference_steps, 51)
 
 latents = 1 / 0.18215 * latents
 
-with open("tensors/latents_7667_final", "rb") as f:
+vae, vae_params = AutoencoderKL.from_pretrained(f"{flax_path}/vae", _do_init=False, dtype=dtype)
+images = vae.decode(latents, params=vae_params)
+
+# convert images to PIL images
+images = images / 2 + 0.5
+images = jnp.clip(images, 0, 1)
+images = (images * 255).round().astype("uint8")
+images = np.asarray(images).reshape((1, 512, 512, 3))
+
+pil_images = [Image.fromarray(image) for image in images]
+pil_images[0].save(f"jax_{device}_fixes.png")
+
+
+with open(f"{tensors_path}/latents_7667_cuda_final", "rb") as f:
     torch_latents = pickle.load(f)
 torch_latents = jnp.transpose(jnp.array(torch_latents), (0, 2, 3, 1))
 
+assert torch_latents.shape == latents.shape, "Wrong shapes"
+
+print(f"Sum: {jnp.sum(jnp.abs((latents - torch_latents)))}")
 print(f"Max: {jnp.max(jnp.abs((latents - torch_latents)))}")
 print(f"Mean: {jnp.mean(jnp.abs((latents - torch_latents)))}")
--- jax-vs-torch/torch-micro-loop.py
+++ jax-vs-torch/torch-micro-loop.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from diffusers import StableDiffusionPipeline, UNet2DConditionModel
 
 device = "cuda"
+tensors_path = "/sddata/sd-tests/tensors"
 
 model_id = "CompVis/stable-diffusion-v1-4"
 pipe = StableDiffusionPipeline.from_pretrained(
@@ -21,11 +22,12 @@ torch.manual_seed(seed)
 latents = torch.randn(
     (1, pipe.unet.in_channels, 512 // 8, 512 // 8),
     generator=None,
-    device=pipe.device,
+    device=device,
 )
+latents = latents.to(device)
 
 Path("tensors").mkdir(exist_ok=True)
-with open(f'tensors/latents_{seed}', 'wb') as f:
+with open(f'{tensors_path}/latents_{seed}_cuda', 'wb') as f:
     pickle.dump(latents.to("cpu"), f)
 
 # Generate text embeddings
@@ -40,21 +42,34 @@ text_input = pipe.tokenizer(
 )
 text_embeddings = pipe.text_encoder(text_input.input_ids.to(pipe.device))[0]
 
-with open(f'tensors/embeddings_{seed}', 'wb') as f:
+with open(f'{tensors_path}/embeddings_{seed}', 'wb') as f:
     pickle.dump(text_embeddings.detach().to("cpu").numpy(), f)
 
 num_inference_steps = 50
 pipe.scheduler.set_timesteps(num_inference_steps, offset=1)
 
 with torch.no_grad():
-    for i, t in tqdm(enumerate(pipe.scheduler.timesteps)):
+    for i, t in enumerate(pipe.scheduler.timesteps):
         # predict the noise residual
         noise_pred = pipe.unet(latents, t, encoder_hidden_states=text_embeddings)["sample"]
 
         # compute the previous noisy sample x_t -> x_t-1
         latents = pipe.scheduler.step(noise_pred, t, latents)["prev_sample"]
+        # Print the axis with dimension 4
+        # print(f"Step {i} [{t}]: noise: {noise_pred.cpu().numpy()[0, 0, 0]}")
+        print(f"Step {i} [{t}]: latents: {latents.cpu().numpy()[0, :, 0, 0]}")
 
     latents = 1 / 0.18215 * latents
 
-with open(f"tensors/latents_{seed}_final", "wb") as f:
+with open(f"{tensors_path}/latents_{seed}_cuda_final", "wb") as f:
     pickle.dump(latents.to("cpu"), f)
+
+with torch.no_grad():
+    image = pipe.vae.decode(latents)
+
+    image = (image / 2 + 0.5).clamp(0, 1)
+    image = image.cpu().permute(0, 2, 3, 1).numpy()
+    image = pipe.numpy_to_pil(image)
+
+image[0].save(f"torch_{device}.png")
+
